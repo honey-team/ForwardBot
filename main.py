c5d7b79787f7c264e46c3fdae2cc7e2c573fbeb2
justdont0 <97027325+justdont0@users.noreply.github.com>
2024-09-04 20:06:17 +0300

    anonymous forwarding

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ async def forward(interaction: discord.Interaction, message: discord.Message):
     await interaction.followup.send('You have already saved a message. Would you like to add it to the list?\n-# NOTE: You can save up to 10 messages.', view=view)
 
 @tree.command(description='Send the saved message(s) to another channel')
-@app_commands.describe(show_original='Whether to show the original message link. Might be needed to set to off on servers with strict automod.')
+@app_commands.describe(show_original='Whether to show the original message link. Might be needed to set to off on servers with strict automod.', anonymous='Whether to send the message anonymously. (in test)')
 @app_commands.user_install()
-async def send(ctx: discord.Interaction, show_original: bool=True):
+async def send(ctx: discord.Interaction, show_original: bool=True, anonymous: bool=False):
     if ctx.user.id not in messages:
         await ctx.response.send_message('You have not saved any messages to send. Use context menu option `Forward`', ephemeral=True)
         return
@@ -62,8 +62,13 @@ async def send(ctx: discord.Interaction, show_original: bool=True):
                 embeds[i].add_field(name=a.filename, value=f'[Link to attachment]({a.url})')
         if show_original:
             embeds[i].add_field(name='Original Message', value=f'[Link to message]({message.jump_url})', inline=False)
-        embeds[i].set_footer(text=f'Forwarded by {ctx.user.name}', icon_url=ctx.user.display_avatar.url)
-    await ctx.response.send_message(embeds=embeds)
+        if not anonymous:
+            embeds[i].set_footer(text=f'Forwarded by {ctx.user.name}', icon_url=ctx.user.display_avatar.url)
+    if anonymous:
+        await ctx.response.send_message('shh...', ephemeral=True)
+        await ctx.followup.send(embeds=embeds)
+    else:
+        await ctx.response.send_message(embeds=embeds)
     del messages[ctx.user.id]
 
 bot.run(getenv('TOKEN'))
